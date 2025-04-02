7c49d8e707df8ede4bbe5f49f1a6352292131814
priscachang <78246888+priscachang@users.noreply.github.com>
2025-04-02 18:28:53 -0400

    Delete env directory

--- env/workers/base_worker.py
+++
@@ -1,6 +0,0 @@
-class BaseWorker:
-    def __init__(self, config=None):
-        self.config = config
-
-    def run(self, task_input):
-        raise NotImplementedError("Each worker must implement the run() method.")
